8953dfbce389454c473fa7aa0ce52436abe77451
Edgardo Martin Gerez <edmargerez@gmail.com>
2020-07-20 12:45:34 -0300

    wip

--- src/routes/muestras.js
+++ src/routes/muestras.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const muestrasController = require('../controllers/muestrasController');
+const searchController = require('../controllers/searchController');
 
 router.get('/new', muestrasController.new);
+
+router.get('/search', searchController.searchCoincidencia);
 
 module.exports = router;
