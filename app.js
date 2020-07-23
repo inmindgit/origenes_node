2de88e7c6bbbfedd2d0ff4eaa3315c041681f225
Edgardo Martin Gerez <edmargerez@gmail.com>
2020-07-23 12:06:56 -0300

    wip

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser')
-
+var passport = require('passport');
 var indexRouter = require('./src/routes/index');
 var usersRouter = require('./src/routes/users');
 var muestrasRouter = require('./src/routes/muestras');
@@ -18,6 +18,8 @@ app.use(session({
   saveUninitialized: true,
   resave: true
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(flash());
 
