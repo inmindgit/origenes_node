2fc470412c378169f15f3f8ff9bfd360769d15d6
Maxi Vadora <maxivadora@gmail.com>
2020-07-23 22:06:50 -0300

    added match controller and views

--- src/controllers/coincidenciasController.js
+++ src/controllers/coincidenciasController.js
@@ -1,4 +1,5 @@
-const askNumberCase = require("../services/askNumberCase");
+const matchStrService = require("../services/matchStrService");
+const matchSnpService = require("../services/matchSnpService");
 
 module.exports = {
   async find(req, res) {
@@ -7,22 +8,63 @@ module.exports = {
     });
   },
 
-  async search(req, res){
+  async snp(req, res) {
+    // definir el nombre de los marcadores SNP (150 marcadores con diferentes valores posibles)
     const {
-      documentId
-    } = req.query;
+      caseNumber
+    } = req.body;
+    marcadores = req.body['marcadores[]'];
 
     const keypair = JSON.parse(process.env.KEYPAIR);
 
-    const result = await askNumberCase.call(keypair, documentId.toString());
+    const result = await matchSnpService.call(
+      keypair,
+      caseNumber,
+      marcadores
+    );
 
+    // redireccionar al usuario al lugar apropiado si esta todo OK, caso contrario mostrar errores.
     if(result.success) {
-      return res.render('muestras/search', {
-        numberCaseResult: result.hash
-      })
+      req.flash('success', `Hash: ${result.hash}`);
+      return res.redirect('/resultados/new');
     } else {
-      return res.render('muestras/find', {
-        numberCaseResult: result.hash
+      req.flash('error', result.message)
+      res.locals.message = req.flash()
+      return res.render('/resultados/new', {
+        error: result.message
+      })
+    }
+  },
+
+  async str(req, res) {
+    const {
+      caseNumber,
+    } = req.body;
+    const strObjects = [];
+    
+    for (i = 1; i < 21; i++) {
+      const marcador = req.body[`marcador[${i}][]`];
+      strObjects.push(marcador);
+    }
+
+    // enviar los datos ingresados al BC
+    const keypair = JSON.parse(process.env.KEYPAIR);
+
+    const result = await matchStrService.call(
+      keypair,
+      caseNumber,
+      strObjects
+    );
+
+    // redireccionar al usuari al lugar apropiado si esta todo OK, caso contrario mostrar errores.
+    if(result.success) {
+      req.flash('success', `Hash: ${result.hash}`);
+      return res.redirect('/resultados/new');
+    } else {
+      req.flash('error', result.message)
+      res.locals.message = req.flash()
+      return res.render('/resultados/new', {
+        error: result.message
       })
     }
   }
--- src/routes/coincidencias.js
+++ src/routes/coincidencias.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const coincidenciasController = require('../controllers/coincidenciasController');
 
 router.get('/find', coincidenciasController.find);
-router.get('/search', coincidenciasController.search);
+router.post('/str', coincidenciasController.str);
+router.post('/snp', coincidenciasController.snp);
 
 module.exports = router;
